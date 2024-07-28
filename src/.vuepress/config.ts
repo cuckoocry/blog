4abcc57c84ca63bd9421daa016fafcc95fb98797
jiang <948742327@qq.com>
2024-07-28 22:52:59 +0800

    增加搜索功能，但是不能使用

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 import theme from "./theme.js";
 
@@ -11,6 +12,24 @@ export default defineUserConfig({
 
   theme,
 
+  // 注意：Algolia DocSearch 是一个免费服务，但您需要通过官方渠道申请以获得应用 ID、API 密钥和索引名称。申请成功后，您才能使用 @vuepress/plugin-docsearch 插件。
+  plugins: [
+    docsearchPlugin({
+      appId: '<APP_ID>',
+      apiKey: '<API_KEY>',
+      indexName: '<INDEX_NAME>',
+      locales: {
+        '/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+            },
+          },
+        },
+      },
+    }),
+  ],
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
